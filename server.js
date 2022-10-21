072e1fe743f232ef7b54ef784d6d0eb64399489b
WiFoDev <lou81804@gmail.com>
2022-10-20 20:33:13 -0500

    add graphql route handler

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log(`Server is running on port 4000`);
+});
